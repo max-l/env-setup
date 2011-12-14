c6518d28c2913eb941618aa69f89edec330b03be
Jacques <jacques@lemieux.cc>
2011-12-13 23:16:17 -0500

    keep

--- build.sh
+++ build.sh
@@ -4,17 +4,22 @@ reldir=`dirname $0`
 cd $reldir
 root=`pwd`
 
-set -o verbose
+function zap {
+  local list=`find . -name $1`
+  for file in $list; do 
+    rm -R $file 
+  done
+}
 
 function process {
   echo "Directory >>>> $1" 
   cd $root/$1
-  rm -R `find . -name '*.class' -type f`
-  rm -R `find . -name 'target' -type d`
-  rm -R `find . -name '.target' -type d`
-  rm -R `find . -name 'bin' -type d`
-  rm -R `find . -name '.project' -type f`
-  rm -R `find . -name '.classpath' -type f`
+  zap '*.class -type f'
+  zap 'target -type d'
+  zap '.target -type d'
+  zap 'bin -type d'
+  zap '.project -type f'
+  zap '.classpath -type f'
   java -Xmx512M -jar ~/bin/sbt-launch-0.11.2.jar "set logLevel in Global := Level.Warn" clean publish-local "eclipse skip-root"
 }  
 
