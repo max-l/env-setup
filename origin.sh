6cf8a7032223628cf419b90af5e8632da20d60ea
Maxime Lévesque <maxime.levesque@gmail.com>
2011-12-23 11:53:30 -0500

    Update origin.sh

--- origin.sh
+++ origin.sh
@@ -4,6 +4,7 @@ if [ ! -e "home/interactive-shell.sh" ]
 then
   sbtVersion="0.11.2"
   git clone git@github.com:max-l/env-setup.git home
+  rm -Rf env-setup/.git
   curl -s -o `pwd`/home/bin/sbt-launch.jar "http://repo.typesafe.com/typesafe/ivy-releases/org.scala-tools.sbt/sbt-launch/$sbtVersion/sbt-launch.jar"
 fi
 
