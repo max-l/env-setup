cb5210a286cf3c7be43633f0d9aa4fe2a32f7899
Maxime Lévesque <maxime.levesque@gmail.com>
2011-12-23 14:11:43 -0500

    Update origin.sh

--- origin.sh
+++ origin.sh
@@ -2,12 +2,16 @@
 
 if [ ! -e "home" ]
 then
+
+  SANDBOX_DIR=`pwd`
+
+  echo Will create a sandbox in "$SANDBOX_DIR"
+
   sbtVersion="0.11.2"
   git clone --quiet git@github.com:max-l/env-setup.git home
   rm -Rf home/.git
-  curl -s -o `pwd`/home/bin/sbt-launch.jar "http://repo.typesafe.com/typesafe/ivy-releases/org.scala-tools.sbt/sbt-launch/$sbtVersion/sbt-launch.jar"
+  curl -s -o $SANDBOX_DIR/home/bin/sbt-launch.jar "http://repo.typesafe.com/typesafe/ivy-releases/org.scala-tools.sbt/sbt-launch/$sbtVersion/sbt-launch.jar"
   
-  SANDBOX_DIR=`pwd`
   echo "export STRONGLINKS_SANDBOX_DIR=$SANDBOX_DIR" > home/bin/interactive-shell.sh
   cat home/bin/interactive-shell.template >> home/bin/interactive-shell.sh
 fi
